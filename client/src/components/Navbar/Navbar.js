53270578d0721bba61aeb7be238b1fd51f3a5b7d
peterj330 <peter.a.jarrard@gmail.com>
2018-11-10 10:54:14 -0600

    current weather and hourly forecast data population

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,17 +1,25 @@
 import React from "react";
 import "./Navbar.css";
 
-const Navbar = () => (
+const Navbar = props => {
+
+  return (
+
     <nav>
-    <div className="nav-wrapper">
-      <a href="/" className="brand-logo">Logo</a>
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
-    </div>
-  </nav>
-);
+      <div className="nav-wrapper">
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li><a href="collapsible.html">  <i className="material-icons">person_add</i></a></li>
+        </ul>
+        <form>
+          <div className="input-field">
+            <input id="search" type="search" required />
+            <label className="label-icon" placeholder="City Search" htmlFor="search" ><i className="material-icons">search</i></label>
+            <i className="material-icons">close</i>
+          </div>
+        </form>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;

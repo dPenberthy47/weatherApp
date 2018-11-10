914b44c8e2f2a7e56911be2d9a692e714b33f758
peterj330 <peter.a.jarrard@gmail.com>
2018-11-10 11:16:39 -0600

    poulating data in current weather display and hourly forecast

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -8,23 +8,16 @@ const Navbar = props => {
     <nav>
       <div className="nav-wrapper">
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-
-          <li><a href="collapsible.html">  <i className="material-icons">person_add</i></a></li>
+          <li>
+            <a href="collapsible.html"> <i className="material-icons">person_add</i>
+            </a>
+          </li>
         </ul>
         <form>
           <div className="input-field">
             <input id="search" type="search" required />
             <label className="label-icon" placeholder="City Search" htmlFor="search" ><i className="material-icons">search</i></label>
             <i className="material-icons">close</i>
-
-          <li><a href="collapsible.html">  <i class="material-icons">person_add</i></a></li>
-        </ul>
-        <form>
-          <div class="input-field">
-            <input id="search" type="search" required />
-            <label class="label-icon" placeholder="City Search" for="search" ><i class="material-icons">search</i></label>
-            <i class="material-icons">close</i>
-
           </div>
         </form>
       </div>

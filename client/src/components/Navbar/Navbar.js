6a86d05ddaf13aac30d5baf7fec5323836ccc815
aaronrd83 <aaronrd83@gmail.com>
2018-11-09 23:31:53 -0600

    navbar & search bar updates

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,21 +1,25 @@
 import React from "react";
 import "./Navbar.css";
 
-const Navbar = () => (
+const Navbar = props => {
+
+  return (
+
     <nav>
-    <div className="nav-wrapper">
-<<<<<<< HEAD
-      <a href="/" className="brand-logo">Logo</a>
-=======
-      <a href="#" className="brand-logo">Logo</a>
->>>>>>> 1d637e3182fa82a2e4afb7a52ec93cfb7c035271
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
-    </div>
-  </nav>
-);
+      <div className="nav-wrapper">
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li><a href="collapsible.html">  <i class="material-icons">person_add</i></a></li>
+        </ul>
+        <form>
+          <div class="input-field">
+            <input id="search" type="search" required />
+            <label class="label-icon" placeholder="City Search" for="search" ><i class="material-icons">search</i></label>
+            <i class="material-icons">close</i>
+          </div>
+        </form>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
--- client/src/components/WeatherDisplay/WeatherDisplay.js
+++ client/src/components/WeatherDisplay/WeatherDisplay.js
@@ -1,16 +1,13 @@
 import React from "react";
 import "./WeatherDisplay.css";
-const Jumbotron = ({ children }) => (
+const WeatherDisplay = ({ children }) => (
   <div
     style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
     className="jumbotron"
   >
-<<<<<<< HEAD
   WeatherDisplay
-=======
->>>>>>> 1d637e3182fa82a2e4afb7a52ec93cfb7c035271
     {children}
   </div>
 );
 
-export default Jumbotron;
+export default WeatherDisplay;

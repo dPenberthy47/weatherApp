660c95712cfbb4ac14c4fc74dc46f40e4901f0ca
dPenberthy47 <dp47@tamu.edu>
2018-11-15 20:42:16 -0600

    hourlyWeather

--- client/src/components/HourlyForecast/HourlyForecast.js
+++ client/src/components/HourlyForecast/HourlyForecast.js
@@ -1,12 +1,34 @@
 import React from "react";
 import "./HourlyForecast.css";
+import {Skycons} from 'skycons-ts';
 
-const HourlyForecast = props => (
-    <div>
-    <h5>{props.hourlyTime}</h5>
-    <h6>{props.hourlyTemp}</h6>
-  </div>
+const HourlyForecast = props => {
 
+  const skycons = new Skycons({'color': 'blue'});
+    // on Android, a nasty hack is needed: {'resizeClear': true}
+ 
+    // you can add a canvas by it's ID...
+  skycons.add(props.hourlyIcon, props.hourlyIcon);
+
+  return (
+
+    <div className="hourlyForecast">
+      <h3>
+        <canvas className={props.hourlyIcon} width="75" height="75"></canvas>
+      </h3>
+
+      <h3>
+        {props.hourlyTime}
+      </h3>
+  
+      <h4>
+        {props.hourlyTemp}
+      </h4>
+      
+      <h4>
+        {props.hourlyPrecip}
+      </h4>
+    </div>
 );
-
-export default HourlyForecast;
+  }
+export default HourlyForecast

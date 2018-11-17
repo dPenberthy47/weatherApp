42b1da1d2554d858a5c8d23138620732b8348a27
peterj330 <peter.a.jarrard@gmail.com>
2018-11-17 10:05:57 -0600

    asdf

--- client/src/components/HourlyForecast/HourlyForecast.js
+++ client/src/components/HourlyForecast/HourlyForecast.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./HourlyForecast.css";
+// import Moment from "react-moment";
+
+// var moment = require("moment");
 
 const HourlyForecast = props => (
+    
+    
     <div>
+      
     <h5>{props.hourlyTime}</h5>
-    <h6>{props.hourlyTemp}</h6>
+    <h6>{props.hourlyTemp}°</h6>
   </div>
 
 );
--- client/src/pages/WeatherApp/WeatherApp.js
+++ client/src/pages/WeatherApp/WeatherApp.js
@@ -2,14 +2,17 @@ import React, { Component } from "react";
 import API from "../../utils/API";
 import Header from "../../components/Header";
 import Navbar from "../../components/Navbar";
-import DarkskyMap from 'react-darksky-map';
 import LocationSearch from "../../components/LocationSearch";
 import Jumbotron from "../../components/Jumbotron";
 import WeatherDisplay from "../../components/WeatherDisplay";
 import HourlyForecast from "../../components/HourlyForecast";
+import DarkskyMap from 'react-darksky-map';
 //import Map from "../../components/Map";
 import FiveDayForecast from "../../components/FiveDayForecast";
 import Footer from "../../components/Footer";
+// import Moment from "react-moment";
+
+// var moment = require("moment");
 
 
 class WeatherApp extends Component {
@@ -21,6 +24,8 @@ class WeatherApp extends Component {
         feelsLike: "",
         dailyLow: "",
         dailyHigh: "",
+        dewPoint: "",
+        precipChance: "",
         currentWeather: [],
         hourlyWeather: [],
     };
@@ -52,7 +57,9 @@ class WeatherApp extends Component {
                     feelsLike: res.data.currently.apparentTemperature,
                     dailyLow: res.data.daily.data[0].temperatureLow,
                     dailyHigh: res.data.daily.data[0].temperatureHigh,
-                    hourlyWeather: res.data.hourly.data
+                    hourlyWeather: res.data.hourly.data, 
+                    dewPoint: res.data.currently.dewPoint,
+                    precipChance: res.data.currently.precipProbability,
                 })
                 console.log(`
                 Current Temp: ${this.state.currentTemp} 
@@ -61,6 +68,8 @@ class WeatherApp extends Component {
                 Summary: ${this.state.currentWeather.currently.summary}
                 Daily Low: ${this.state.dailyLow}
                 Daily High: ${this.state.dailyHigh}
+                Dew Point: ${this.state.dewPoint}
+                Precip Chance: ${this.state.precipChance}
                 `)
                 console.log(this.state.currentWeather);
 
@@ -69,17 +78,57 @@ class WeatherApp extends Component {
 
     }; //closes handleFormSubmit
 
+
+    componentWillMount() {
+        API.search(
+            this.state.latitude,
+            this.state.longitude,
+            console.log(`
+            latitude = ${this.state.latitude}
+            longitude = ${this.state.longitude}
+            `)
+
+        )
+            .then(res => {
+                this.setState({
+                    currentWeather: res.data,
+                    image: res.data.currently.icon,
+                    currentTemp: res.data.currently.temperature,
+                    feelsLike: res.data.currently.apparentTemperature,
+                    dailyLow: res.data.daily.data[0].temperatureLow,
+                    dailyHigh: res.data.daily.data[0].temperatureHigh,
+                    hourlyWeather: res.data.hourly.data,
+                    dewPoint: res.data.currently.dewPoint,
+                    precipChance: res.data.currently.precipProbability,
+                })
+                console.log(`
+                Current Temp: ${this.state.currentTemp} 
+                Feels Like: ${this.state.feelsLike}
+                Time: ${this.state.currentWeather.currently.time}
+                Summary: ${this.state.currentWeather.currently.summary}
+                Daily Low: ${this.state.dailyLow}
+                Daily High: ${this.state.dailyHigh}
+                Dew Point: ${this.state.dewPoint}
+                Precip Chance: ${this.state.precipChance}
+                `)
+                console.log(this.state.currentWeather);
+
+            })
+            .catch(error => console.log(error));
+    };
+
     render() {
         return (
             <div>
                 <Header />
                 <Navbar />
+                
                 <LocationSearch
                     value={this.state.search}
                     handleInputChange={this.handleInputChange}
                     handleFormSubmit={this.handleFormSubmit}
                 />
-                {/* <Card image={this.state.image} /> */}
+
                 <Jumbotron>
                     <WeatherDisplay
                         image={this.state.image}
@@ -90,17 +139,21 @@ class WeatherApp extends Component {
                     />
 
                 </Jumbotron>
+
                 {this.state.hourlyWeather.map(hourly => (
                     <HourlyForecast
-                         hourlyTime={hourly.time}
+                        hourlyTime={hourly.time}
                         hourlyTemp={hourly.temperature}
                       />
-
-                    
                 ))}
 
+               
                <DarkskyMap lat={this.state.latitude} lng={this.state.longitude} zoom={8} mapField="temp" />
+               
+               <Jumbotron>
                 <FiveDayForecast />
+                </Jumbotron>
+
                 <Footer />
             </div>
         )
